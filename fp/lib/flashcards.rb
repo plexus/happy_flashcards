320b4dbc1c665f25516519b1da6986bcced74dae
Arne Brasseur <arne@arnebrasseur.net>
2016-02-20 07:16:10 +1000

    Add hamster and refactor

--- fp/lib/flashcards.rb
+++ fp/lib/flashcards.rb
@@ -1,13 +1,15 @@
 require 'csv'
 require 'pathname'
 require 'attribs'
+require 'hamster'
 
 module Flashcards
   TEN_MINUTES = 10*60
   ONE_DAY = 24*60*60
+  Vector = Hamster::Vector
 
   def self.load_csv(input)
-    Deck.new(cards: CSV.open(input).map {|f,b| Card.create(f, b) })
+    Deck.new(CSV.open(input).map {|f,b| Card.new(front: f, back: b) })
   end
 
   class Deck
@@ -16,32 +18,16 @@ module Flashcards
 
     def_delegators :cards, :first, :size
 
-    def replace(old_card, new_card)
-      with(
-        cards: cards.map do |c|
-          if c.equal?(old_card)
-            new_card
-          else
-            c
-          end
-        end
-      )
+    def initialize(cards)
+      super(cards: Vector.new(cards))
     end
 
     def answer_correct(card)
-      replace(card, card.with_interval(ONE_DAY))
+      cards.put(cards.index(card), card.with(interval: ONE_DAY))
     end
   end
 
   class Card
-    include Attribs.new(:front, :back, :interval)
-
-    def self.create(front, back)
-      new(front: front, back: back, interval: TEN_MINUTES)
-    end
-
-    def with_interval(i)
-      with(interval: i)
-    end
+    include Attribs.new(:front, :back, interval: TEN_MINUTES)
   end
 end
