255b31d203cdc409cff7b9d585acd36b1214da8c
Arne Brasseur <arne@arnebrasseur.net>
2016-02-21 21:04:06 +1100

    Introduce a session object to hold state

--- shared/gui.rb
+++ shared/gui.rb
@@ -14,12 +14,7 @@ require "flashcards"
 require_relative "persistence"
 
 require "shoes"
-
-EXAMPLE_DECK = if ARGV[1]
-                 Pathname(ARGV[1]).expand_path
-               else
-                 Pathname(__FILE__).dirname.join("example_deck.csv")
-               end
+require "attribs"
 
 NBSP="\u00A0"
 
@@ -37,25 +32,62 @@ def print_inspect!(deck)
   end
 end
 
-Shoes.app do
-  @deck = Flashcards.load_csv(EXAMPLE_DECK)
+
+class Session
+  include Attribs.new(:location, :deck, current_card: nil)
+
+  def self.open(file)
+    new(location: file, deck: Flashcards.load_deck(file))
+  end
+
+  def close
+    Flashcards.save_deck(deck, location)
+  end
 
   def next!
-    print_inspect!(@deck)
+    @current_card = deck.next(Time.now)
+  end
 
-    @card = @deck.next(Time.now)
+  def answer_right!
+    @deck = deck.answer_right(current_card, Time.now)
+  end
 
-    if @card
-      @front.text = @card.front
-      @back.text = NBSP
-      @show.show
-      [@right, @wrong, @recheck].each(&:hide)
+  def answer_wrong!
+    @deck = deck.answer_wrong(current_card, Time.now)
+  end
+end
+
+
+Shoes.app do
+  def card
+    @session.current_card
+  end
+
+  def session
+    @session
+  end
+
+  def next!
+    if session
+      print_inspect!(session.deck)
+
+      session.next!
+
+      if card
+        @front.text = card.front
+        @back.text = NBSP
+        @show.show
+        [@right, @wrong, @recheck].each(&:hide)
+      else
+        @front.text = "- done -"
+        @back.text = NBSP
+        [@right, @wrong, @show].each(&:hide)
+        @recheck.show
+      end
     else
-      @front.text = "- done -"
-      @back.text = NBSP
-      [@right, @wrong, @show].each(&:hide)
-      @recheck.show
+      [@show, @right, @wrong, @recheck].each(&:hide)
     end
+
   end
 
   stack margin: 30 do
@@ -64,18 +96,18 @@ Shoes.app do
 
     flow do
       @show = button "Show", margin: 5 do
-        @back.text = @card.back
+        @back.text = card.back
         @show.hide
         [@right, @wrong].each(&:show)
       end
 
       @right = button "Right", margin: 5 do
-        @deck = @deck.answer_right(@card, Time.now)
+        session.answer_right!
         next!
       end
 
       @wrong = button "Wrong", margin: 5 do
-        @deck = @deck.answer_wrong(@card, Time.now)
+        session.answer_wrong!
         next!
       end
 
@@ -85,30 +117,48 @@ Shoes.app do
     end
 
     flow do
-      @load_csv = button "Import CSV" do
-        fname = ask_open_file
-        if fname
-          @deck = Flashcards.load_csv(fname)
-        end
-        next!
-      end
-
       @load_deck = button "Load deck" do
         fname = ask_open_file
-        if fname
-          @deck = Flashcards.load_deck(fname)
+
+        if @session
+          @session.close
+          @session = nil
         end
+
+        if fname
+          @session = Session.open(fname)
+        end
+
         next!
       end
 
-      @save_deck = button "Save deck" do
-        fname = ask_save_file
-        if fname
-          Flashcards.save_deck(@deck, fname)
+      @load_csv = button "Import CSV" do
+        fname = ask_open_file
+
+        if @session
+          @session.close
+          @session = nil
         end
+
+        if fname
+          @session = Session.new(location: fname + ".html", deck: Flashcards.load_csv(fname))
+        end
+        next!
+
       end
+
+      # @save_deck = button "Save deck" do
+      #   fname = ask_save_file
+      #   if fname
+      #     Flashcards.save_deck(@deck, fname)
+      #   end
+      # end
     end
   end
 
   next!
+
+  at_exit do
+    @session.close
+  end
 end
